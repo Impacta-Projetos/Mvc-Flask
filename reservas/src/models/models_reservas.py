6b6e1f883b292c0d163db22a00a91a8e2d0a00ca
Iago Rozales Mahiques <iagomahiques@gmail.com>
2025-11-03 10:52:21 -0300

    Add files via upload

--- reservas/src/models/models_reservas.py
+++ reservas/src/models/models_reservas.py
@@ -1,5 +1,8 @@
 from . import banco_res
 
 class Reservas(banco_res.Model):
-    #TODO: definir as colunas da tabela reservas
-    pass
+    id = banco_de_dados.Column(banco_de_dados.Integer, primary_key=True)
+    num_sala = banco_de_dados.Column(banco_de_dados.Integer, nullable=False)
+    lab = banco_de_dados.Column(banco_de_dados.Boolean, nullable=False)
+    data = banco_de_dados.Column(banco_de_dados.Date, nullable=False)
+    turma_id = banco_atv.Column(banco_atv.Integer, nullable=False)
